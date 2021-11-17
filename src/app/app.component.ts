6b5c9b7eb4e3db020e2e795d22b1e32d5a845c9d
welinton <welinton.b.borges@gmail.com>
2021-11-17 15:04:31 -0300

    refatoração

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import  { JogoService } from'./pages/jogo/shared/jogo.service'
+
 
 @Component({
   selector: 'app-root',
